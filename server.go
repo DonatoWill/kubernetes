ea932a85c4be560330618a0471738ab13315e757
DonatoWill <donato_will@hotmail.com>
2022-04-21 16:57:14 -0300

    Adicionando config, secret e livenessProbe

--- server.go
+++ server.go
@@ -1,12 +1,59 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"time"
+)
+
+var startedAt = time.Now()
 
 func main() {
+	http.HandleFunc("/healthz", Healthz)
+	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8081", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello, World!!!</h1>"))
+
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	
+	fmt.Fprintf(w, "Hello %s, I am %s years old!", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+	if err != nil {
+		log.Fatal("Error reading file:", err)
+	}
+	fmt.Fprintf(w, "My Family: %s.", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	
+	fmt.Fprintf(w, "User: %s. Password: %s.", user, password)
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() > 25 {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("Server is not ready. It has been up for %s.", duration)))
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Server is ready. It has been up for %s.", duration)))
+	}
+
 }
